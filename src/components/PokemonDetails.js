7a2ad6e6896583f46a19ae5f88ad0e26a96e2a95
mmsmsy <mmsmsy@gmail.com>
2017-05-18 11:54:04 +0200

    Updated SASS styles, got rid of most duplication in CSS file, got rid of styling in JS files

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -50,20 +50,15 @@ class PokemonDetails extends Component{
     pokemon.types.map(types => pokemonType += types.type.name + ' / ');
     pokemonType = pokemonType.slice(0,-3);
 
-    const styles = {
-      'textTransform': 'capitalize'
-    }
     return (
       <div id="pokemon--details">
         <div className="nav">
           <Link className="nav-back-to-list" to="/">Back to the list</Link>
-          <Link className="nav-prev" to={`/pokemons/${outOfRange(id,-1)}`}>Prev</Link>
-          <Link className="nav-next" to={`/pokemons/${outOfRange(id,1)}`}>Next</Link>
         </div>
         <div id="pokemon--details--sprite">
           <img src={`/spriteshq/${id}.png`} alt={pokemon.name} />
         </div>
-        <table style={styles}>
+        <table>
           <tbody><tr><td>#</td><td>{pokemon.id}</td></tr></tbody>
           <tbody><tr><td>Name</td><td>{pokemon.name}</td></tr></tbody>
           <tbody><tr><td>Type</td><td>{pokemonType}</td></tr></tbody>
@@ -71,6 +66,12 @@ class PokemonDetails extends Component{
           <tbody><tr><td>Weight</td><td>Avg. {parseFloat(Math.round(pokemon.weight) / 10).toFixed(1)}kg ({parseFloat(Math.round(pokemon.weight * 2) / 100).toFixed(2)} - {parseFloat(Math.round(pokemon.weight*18) / 100).toFixed(2)})</td></tr></tbody>
           <tbody><tr><td>Height</td><td>Avg. {pokemon.height*10}cm ({pokemon.height*6} - {pokemon.height*14})</td></tr></tbody>
         </table>
+        <div className="shrink-nav">
+          <div className="nav">
+            <Link className="nav-prev" to={`/pokemons/${outOfRange(id,-1)}`}>Prev</Link>
+            <Link className="nav-next" to={`/pokemons/${outOfRange(id,1)}`}>Next</Link>
+          </div>
+        </div>
       </div>
     )
   }
